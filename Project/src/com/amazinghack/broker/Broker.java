f95259b02b43eec407ed754c3e45136ba1a179ac
Balaji S <balaaagi@gmail.com>
2015-09-19 22:42:56 +0530

    Initial Version of Broker

--- Project/src/com/amazinghack/broker/Broker.java
+++ Project/src/com/amazinghack/broker/Broker.java
@@ -8,52 +8,86 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Set;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import java.util.*;
+
 public class Broker {
 
-	public void notifySubscribers(HashMap<String,ArrayList<Integer>> modifiedFields) throws ParseException {
-		Set<String> fieldList = modifiedFields.keySet();
-		for(String fieldName:fieldList){
-			ArrayList<Integer> modifiedItems = modifiedFields.get(fieldName);
-			ArrayList<HashMap<String,Criteria>> fieldMap = fieldSubscription.get(fieldName);
-			for(HashMap<String, Criteria> subscriberCriteria:fieldMap){
-				validateCriteria(modifiedItems,subscriberCriteria,fieldName);
-			}
-		}
-	}
-	
-	private void validateCriteria(ArrayList<Integer> modifiedItems,
-			HashMap<String,Criteria> subscriberCriteria, String fieldName) throws ParseException {
-		// TODO Auto-generated method stub
-		for(Integer item:modifiedItems) {
+  private static final String EXCHANGE_NAME = "topic_logs";
 
-			HashMap<Integer,ArrayList<String>> subscriberList = new HashMap<Integer, ArrayList<String>>();
-			subscriberList.put(item, new ArrayList<String>());
-			
-			HashMap<String,String> itemData = dataValues.get(item);
-			String itemFieldValue = itemData.get(fieldName);
+  public static void main(String[] argv) {
+    Connection connection = null;
+    Channel channel = null;
+    try {
+      ConnectionFactory factory = new ConnectionFactory();
+      factory.setHost("localhost");
 
-			for(String subscriberName:subscriberCriteria.keySet()) {
-				Criteria subscriptionCriteria = subscriberCriteria.get(subscriberName);
-				if(subscriptionCriteria.isConditionTrueFor(fieldName, itemFieldValue)){
-					subscriberList.get(item).add(subscriberName);
-				}
-			}
-			if(subscriberList.get(item).size()>0) {
-				addToQueue(subscriberList);
-			}
-		}
-	}
+      connection = factory.newConnection();
+      channel = connection.createChannel();
 
-	private void addToQueue(HashMap<Integer,ArrayList<String>> subscriberList) {
-		// TODO Auto-generated method stub
-		if(notificationQueue==null){
-			notificationQueue = new LinkedList<HashMap<Integer, ArrayList<String>>>();
-		}
-		try {
-			notificationQueue.add(subscriberList);
-		}
-		catch(IllegalStateException e){
-			System.out.println("notificationQueue out of space");
-		}
-	}
+      channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+      Integer productId=12788;
+      HashMap<String,Object> attributes=new HashMap<String,Object>();
+      attributes.put("LISTPRICE",123.45);
+      attributes.put("AUTHOR","ChetanBhagat");
+      attributes.put("TITLE","TWOSTATES");
+      attributes.put("DATE","21122010");
+      attributes.put("PUBLISHER","RaiPublications");
+     
+    
+      String routingKey = frameRoutingKey(attributes,productId);
+      String message = productId.toString();
+
+      channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
+      System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+
+    }
+    catch  (Exception e) {
+      e.printStackTrace();
+    }
+    finally {
+      if (connection != null) {
+        try {
+          connection.close();
+        }
+        catch (Exception ignore) {}
+      }
+    }
+  }
+
+  private static String getRouting(String[] strings){
+    if (strings.length < 1)
+    	    return "anonymous.info";
+    return strings[0];
+  }
+
+  public static String frameRoutingKey(HashMap<String,Object> attributes,Integer productId){
+    String outputMessage=productId.toString();
+    Set<String> productAttributes=attributes.keySet();
+    outputMessage=outputMessage+"."+attributes.get("TITLE");
+    outputMessage=outputMessage+"."+attributes.get("AUTHOR");
+    outputMessage=outputMessage+"."+attributes.get("DATE").toString();
+    outputMessage=outputMessage+"."+attributes.get("PUBLISHER");
+    outputMessage=outputMessage+"."+attributes.get("LISTPRICE").toString();
+    return outputMessage;
+  } 
+
+  private static String getMessage(String[] strings){
+    if (strings.length < 2)
+    	    return "Hello World!";
+    return joinStrings(strings, " ", 1);
+  }
+
+  private static String joinStrings(String[] strings, String delimiter, int startIndex) {
+    int length = strings.length;
+    if (length == 0 ) return "";
+    if (length < startIndex ) return "";
+    StringBuilder words = new StringBuilder(strings[startIndex]);
+    for (int i = startIndex + 1; i < length; i++) {
+        words.append(delimiter).append(strings[i]);
+    }
+    return words.toString();
+  }
 }
